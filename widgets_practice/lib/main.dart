e06c5b1026085fcde2cae4430af9e173c26d9ad8
KChristelle <r.ayateke@alustudent.com>
2021-01-26 10:11:24 +0200

    Added Floating Action Button

--- widgets_practice/lib/main.dart
+++ widgets_practice/lib/main.dart
@@ -6,89 +6,100 @@ void main() {
   ));
 }
 
-class UserCard extends StatelessWidget {
+class UserCard extends StatefulWidget {
+  @override
+  _UserCardState createState() => _UserCardState();
+}
+
+class _UserCardState extends State<UserCard> {
+  int userAge = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('Widget Project'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0,40.0,30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-              backgroundImage: AssetImage('assets/me.jpeg'),
-              radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[800],
-            ),
-            Text(
-              'Name',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Rachel Ayateke',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'Age',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              '20',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('Widget Project'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                userAge += 1; 
+              });
+            },
+            child: Icon(Icons.add),
+            backgroundColor: Colors.grey[800]),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/me.jpeg'),
+                  radius: 40.0,
                 ),
-                SizedBox(width: 10.0),
-                Text(
-                  'r.ayateke@gmail.com',
-                  style: TextStyle(
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey[800],
+              ),
+              Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Rachel Ayateke',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                'Age',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '$userAge',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
                     color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(width: 10.0),
+                  Text(
+                    'r.ayateke@gmail.com',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        )
-    );
+        ));
   }
 }
-
